f63eb7ed9e67fbb1a5650b9cb3c340283aed081c
willyoliv <willyoliversilva@gmail.com>
2022-07-29 19:15:31 -0300

    criando configurações iniciais para usar o hook Feed V3

--- awsDynamo.js
+++ awsDynamo.js
@@ -1,72 +1,56 @@
-import dynamo from 'dynamodb';
-import AWS from 'aws-sdk';
-import dotenv from 'dotenv';
+import dynamo from "dynamodb";
+import AWS from "aws-sdk";
+import dotenv from "dotenv";
+import { generateUUID } from "./util/uuid-generator.js";
+
 dotenv.config();
 
 dynamo.AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: process.env.AWS_DEFAULT_REGION,
-  endpoint:"http://dynamodb.us-east-1.amazonaws.com",
+  endpoint: process.env.AWS_ENDPOINT,
 });
 
-console.log("dynamo.js")
+console.log("dynamo.js");
 const dynamoClient = new AWS.DynamoDB.DocumentClient();
 const TABLE_NAME = "vtex_products";
 
 const getProdutos = async () => {
-    /*const params = {
-        TableName: TABLE_NAME,
-
-    };
-    console.log("produtos");
-    const produtos = await dynamoClient.scan(params).promise();
-	console.table("produtos",produtos);
-
-    return produtos;*/
-
-
-	var docClient = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
-
-	var params = {
- 	 TableName: TABLE_NAME,
- 	 Key: {'id':"1"}
-	};
-
-	docClient.get(params, function(err, data) {
-  	 if (err) {
-    	 console.log("Error", err);
-  	} else {
-   	 console.log("Success", data.Item);
-	 return data.Item;
-  	}
-	});
-	var docClient = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
-	}
+  const params = {
+    TableName: TABLE_NAME,
+  };
+  return await dynamoClient.scan(params).promise();
+};
 
 const addProduto = async (produto) => {
+  produto.id = generateUUID();
+  const params = {
+    TableName: TABLE_NAME,
+    Item: produto,
+  };
+
+  return await dynamoClient.put(params).promise();
+};
+
+const updateProduto = async (produto) => {
+    produto.quantidade++;
     const params = {
         TableName: TABLE_NAME,
-        Item: produto
-    };
+        Item: produto,
+      };
+    
+      return await dynamoClient.put(params).promise();
+};
 
-    return await dynamoClient.put(params).promise();
-}
+const getProdutoById = async (sku) => {
+  const params = {
+    TableName: TABLE_NAME,
+    FilterExpression: "sku = :sku",
+    ExpressionAttributeValues: { ":sku": parseInt(sku) },
+  };
 
-const getProdutoById = async (id) => {
-    const params = {
-        TableName: TABLE_NAME,
-        key: {
-            id
-        }
-    };
+  return await dynamoClient.scan(params).promise();
+};
 
-    return await dynamoClient.get(params).promise();
-}
-
-  export{
-    dynamoClient,
-    getProdutos,
-    getProdutoById,
-    addProduto
- }
+export { dynamoClient, getProdutos, getProdutoById, addProduto, updateProduto };
